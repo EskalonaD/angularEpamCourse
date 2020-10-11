e6f359bb836387c90c4bc612e3144552999612bf
Denys Eskalona-Lopes <denys_eskalona-lopes@epam.com>
2020-10-11 13:56:16 +0300

    create first component

---
+++ changelog.md
@@ -0,0 +1,1 @@
+- First Component were created
---
+++ src/app/first-component/first-component.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+enum ProductCategory {
+  Cars = 'Машины',
+  Courses = 'Онлайн Курсы',
+  Appartments = 'Квартиры',
+  Trends = 'Популярные товары',
+}
+
+@Component({
+  selector: 'app-first-component',
+  templateUrl: './first-component.component.html',
+  styleUrls: ['./first-component.component.scss']
+})
+export class FirstComponentComponent implements OnInit {
+  name: string;
+  description: string;
+  price: number;
+  category: typeof ProductCategory;
+  isAvailable: boolean;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.name = 'Shop';
+    this.description = 'Лучший магазин для всех';
+    this.category = ProductCategory;
+    this.isAvailable = false;
+  }
+
+}
