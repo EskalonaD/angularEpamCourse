e7bd69ef99dd072a0e720462b037da262a1def25
EskalonaD <mrragday@gmail.com>
2020-10-25 22:09:02 -0700

    Components: task 11 (add app title)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  @ViewChild('appTitle') appTitle: ElementRef;
+
   title = 'shop';
   shouldShowCart: boolean;
   cartEmpty: boolean;
+
+  ngAfterViewInit() {
+    this.appTitle.nativeElement.textContent = this.title;
+  }
 
   showCart(): void {
     this.shouldShowCart = true;
